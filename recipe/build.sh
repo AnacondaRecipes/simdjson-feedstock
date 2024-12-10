ad935b70d0ab2a43825025d1ce35a084bc345b58
Charles Bousseau <cbousseau@anaconda.com>
2024-12-10 14:16:40 -0500

    adjust build scripts

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,7 @@ cmake -B build/ \
     -D SIMDJSON_DEVELOPER_MODE=OFF \
     -D SIMDJSON_BUILD_STATIC_LIB=ON \
     -D BUILD_SHARED_LIBS=ON \
+    -D CMAKE_BUILD_TYPE=Release \
     ${CMAKE_ARGS}
 
 cmake --build build/ --parallel "${CPU_COUNT}" --verbose
